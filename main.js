e6e046133256034e771401394b76a529abc8067c
KirklandGee <91338420+KirklandGee@users.noreply.github.com>
2021-12-07 15:32:08 -0500

    Main commit

--- main.js
+++ main.js
@@ -10,17 +10,14 @@ const generateHaiku = () => {
     let line3 = haikuLines.possibleLine3[Math.floor(Math.random() * haikuLines.possibleLine1.length)]
     let newHaiku= [line1, line2, line3]
     newHaiku.forEach(line => {
-        let divLine = document.createElement("div")
+        let divLine = document.createElement("p")
         let currLine = document.createTextNode(line)
         divLine.appendChild(currLine)
-        const currentDiv = document.getElementById("haikuLine")
+        const currentDiv = document.createElement("div")
         document.body.insertBefore(currentDiv, currLine)
-        console.log(line)
+        console.log(linde)
     })
     return newHaiku
 }
-
-generateHaiku()
-
 
 document.getElementById("haikuGenerator").onclick = generateHaiku()
